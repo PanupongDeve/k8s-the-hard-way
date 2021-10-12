498519115d36b0d93c8043ae4b844ba2d727e16f
Panupong Chamsomboon <5635512110@psu.ac.th>
2021-10-13 00:33:05 +0700

    add client certificate

---
+++ 1-provision-cert/1-provision-CA-Cert/ืnote.md
@@ -0,0 +1,6 @@
+./1-install-CA-tools.sh <br /><br />
+
+mkdir k8s-hardware
+cd k8s-hardware
+
+../2-generate-CA-cert.sh
---
+++ 1-provision-cert/2-generate-client-cert/2-generate-k8s-client-cert.sh
@@ -0,0 +1,63 @@
+# WORKER0_HOST=<Public hostname of your first worker node cloud server>
+WORKER0_HOST=a56bd118ff1c.mylabserver.com
+# WORKER0_IP=<Private IP of your first worker node cloud server>
+WORKER0_IP=172.31.26.49
+# WORKER1_HOST=<Public hostname of your second worker node cloud server>
+WORKER1_HOST=3ab18b3b921c.mylabserver.com
+# WORKER1_IP=<Private IP of your second worker node cloud server>
+WORKER1_IP=13.212.150.251
+
+{
+cat > ${WORKER0_HOST}-csr.json << EOF
+{
+"CN": "system:node:${WORKER0_HOST}",
+"key": {
+"algo": "rsa",
+"size": 2048
+},
+"names": [
+{
+"C": "US",
+"L": "Portland",
+"O": "system:nodes",
+"OU": "Kubernetes The Hard Way",
+"ST": "Oregon"
+}
+]
+}
+EOF
+cfssl gencert \
+-ca=ca.pem \
+-ca-key=ca-key.pem \
+-config=ca-config.json \
+-hostname=${WORKER0_IP},${WORKER0_HOST} \
+-profile=kubernetes \
+${WORKER0_HOST}-csr.json | cfssljson -bare ${WORKER0_HOST}
+
+
+cat > ${WORKER1_HOST}-csr.json << EOF
+{
+"CN": "system:node:${WORKER1_HOST}",
+"key": {
+"algo": "rsa",
+"size": 2048
+},
+"names": [
+{
+"C": "US",
+"L": "Portland",
+"O": "system:nodes",
+"OU": "Kubernetes The Hard Way",
+"ST": "Oregon"
+}
+]
+}
+EOF
+cfssl gencert \
+-ca=ca.pem \
+-ca-key=ca-key.pem \
+-config=ca-config.json \
+-hostname=${WORKER1_IP},${WORKER1_HOST} \
+-profile=kubernetes \
+${WORKER1_HOST}-csr.json | cfssljson -bare ${WORKER1_HOST}
+}
