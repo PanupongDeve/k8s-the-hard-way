3a64c1f20ebe9bacfdc037fadb0f0315396a0459
Panupong Chamsomboon <5635512110@psu.ac.th>
2021-10-13 09:41:04 +0700

    add script send key to server

---
+++ 1-provision-cert/3-generate-k8s-api-server-cert/2-send-k8s-api-key-to-controller.sh
@@ -0,0 +1,6 @@
+# scp ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem user@<controller 1 public IP>:~/
+scp ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem cloud_user@a56bd118ff1c.mylabserver.com:~/
+
+# scp ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem user@<controller 2 public IP>:~/
+scp ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem cloud_user@3ab18b3b921c.mylabserver.com:~/
+
---
+++ 1-provision-cert/4-generate-service-account-key-pair/2-send-SA-key-to-controller.sh
@@ -0,0 +1,6 @@
+# scp service-account-key.pem service-account.pem user@<controller 1 public IP>:~/
+scp service-account-key.pem service-account.pem cloud_user@a56bd118ff1c.mylabserver.com:~/
+
+# scp service-account-key.pem service-account.pem user@<controller 2 public IP>:~/
+scp service-account-key.pem service-account.pem cloud_user@3ab18b3b921c.mylabserver.com:~/
+
