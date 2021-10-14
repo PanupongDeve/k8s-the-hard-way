48fbaf9ff063213039bde6bf1e3bbd2f4e2c741a
Panupong Chamsomboon <5635512110@psu.ac.th>
2021-10-14 08:48:14 +0700

    add script install etcd cluster

---
+++ 4-install-etcd-cluster/2-setup-systemd.sh
@@ -0,0 +1,42 @@
+# ETCD_NAME=<cloud server hostname>
+ETCD_NAME=<cloud server hostname>
+INTERNAL_IP=<private ip>
+# INITIAL_CLUSTER=<controller 1 hostname>=https://<controller 1 private ip>:2380,<controller 2hostname>=https://<controller 2 private ip>:2380
+INITIAL_CLUSTER=a56bd118ff1c.mylabserver.com=https://172.31.26.49:2380,3ab18b3b921c.mylabserver.com=https://172.31.31.44:2380
+
+cat << EOF | sudo tee /etc/systemd/system/etcd.service
+[Unit]
+Description=etcd
+Documentation=https://github.com/coreos
+
+[Service]
+ExecStart=/usr/local/bin/etcd \\
+    --name ${ETCD_NAME} \\
+    --cert-file=/etc/etcd/kubernetes.pem \\
+    --key-file=/etc/etcd/kubernetes-key.pem \\
+    --peer-cert-file=/etc/etcd/kubernetes.pem \\
+    --peer-key-file=/etc/etcd/kubernetes-key.pem \\
+    --trusted-ca-file=/etc/etcd/ca.pem \\
+    --peer-trusted-ca-file=/etc/etcd/ca.pem \\
+    --peer-client-cert-auth \\
+    --client-cert-auth \\
+    --initial-advertise-peer-urls https://${INTERNAL_IP}:2380 \\
+    --listen-peer-urls https://${INTERNAL_IP}:2380 \\
+    --listen-client-urls https://${INTERNAL_IP}:2379,https://127.0.0.1:2379 \\
+    --advertise-client-urls https://${INTERNAL_IP}:2379 \\
+    --initial-cluster-token etcd-cluster-0 \\
+    --initial-cluster ${INITIAL_CLUSTER} \\
+    --initial-cluster-state new \\
+    --data-dir=/var/lib/etcd
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable etcd
+sudo systemctl start etcd
+sudo systemctl status etcd
+
---
+++ 4-install-etcd-cluster/4-send-script-to-controller.sh
@@ -0,0 +1,4 @@
+
+scp *.sh cloud_user@a56bd118ff1c.mylabserver.com:~/
+
+scp *.sh cloud_user@3ab18b3b921c.mylabserver.com:~/
